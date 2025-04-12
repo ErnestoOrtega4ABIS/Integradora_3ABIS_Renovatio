eb3000001064b67e1798e35370d948cfdf4a5b9b
ErnestoOrtega4ABIS <ernestoortega1423@gmail.com>
2025-04-12 15:01:52 -0600

    Delete frontend directory

--- frontend/app_web/js/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-export const apiURL_project = 'https://renovatiogc.up.railway.app/api/projects';
-export const apiURL_client = 'https://renovatiogc.up.railway.app/api/clients';
-export const apiURL_employee = 'https://renovatiogc.up.railway.app/api/employees';
-export const apiURL_assignment = 'https://renovatiogc.up.railway.app/api/assignments';
-export const apiURL_credentials = 'https://renovatiogc.up.railway.app/api/credentials';
